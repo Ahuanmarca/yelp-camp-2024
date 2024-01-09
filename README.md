e573e753c5b0cd5057959c3cf81c317c5e32e193
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-01-09 07:45:53 +0100

    Added basic error handling. Custom ExpressError, try/catch blocks, error.ejs template, app.use to catch errors passed to next()

--- README.md
+++ README.md
@@ -14,3 +14,17 @@ Web App created with **Node.js** and **MongoDB**.
 | /campgrounds/:id/delete | DELETE | Deletes one campground            |
 | /campgrounds/new        | GET    | Form to create new campground     |
 | /campgrounds/new        | POST   | (Endpoint) Creates new campground |
+
+## Error Handling / Form Validation
+
+- Routes have (req, res, next) signature.
+- try/catch blocks inside routes.
+  - Some errors will be thrown by node/express.
+  - Some errors must be manually thrown (i.e. trying to create an empty campground).
+- Using custom ExpressError for manually thronwn errors.
+- At the bottom of app.js we have an app.use that catches all errors passed to next() in the routes.
+- We are using an error.ejs template to show errors in a more friendly way.
+
+- For the client side form validation we are using Bootstrap.
+- TODO: For server side, we must throw errors when needed.
+- TODO: Use JOI for server side form validation.
--- app.js
+++ app.js
@@ -3,12 +3,21 @@ import dbConnect from "./config/database.js";
 import express from "express";
 import Campground from "./models/campgrounds.js";
 import methodOverride from "method-override";
+import ExpressError from "./src/utils/ExpressError.js";
 import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 main().catch((err) => console.log(err));
+
+/**
+ * ExpressError
+ * I can throw ExpressError inside try/catch blocks (on the try 
+ * section) to catch errors that won't be automatically detected 
+ * by node, like creating an empty campground. It will pass to
+ * next() on the catch block, then to app.use at the bottom.
+ */
 
 async function main() {
   setLocalEnvironment();
@@ -35,10 +44,15 @@ async function main() {
   });
 
   // * CAMPGROUND DETAILS
-  app.get("/campgrounds/:id/show", async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    res.render("campgrounds/show", { campground });
+  app.get("/campgrounds/:id/show", async (req, res, next) => {
+    try {
+      // throw new ExpressError("OOPS, ERROR MANUALLY THRONW", 404);
+      const { id } = req.params;
+      const campground = await Campground.findById(id);
+      res.render("campgrounds/show", { campground });
+    } catch (e) {
+      next(e);
+    }
   });
 
   // * CREATE NEW CAMPGROUND
@@ -46,41 +60,66 @@ async function main() {
     res.render("campgrounds/new");
   });
 
-  app.post("/campgrounds/new", async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    console.log(campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}/show`);
+  app.post("/campgrounds/new", async (req, res, next) => {
+    try {
+      const campground = new Campground(req.body.campground);
+      await campground.save();
+      res.redirect(`/campgrounds/${campground._id}/show`);
+    } catch (e) {
+      next(e);
+    }
   });
 
   // * EDIT CAMPGROUND
-  app.get("/campgrounds/:id/edit", async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    res.render("campgrounds/edit", { campground });
+  app.get("/campgrounds/:id/edit", async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const campground = await Campground.findById(id);
+      res.render("campgrounds/edit", { campground });
+    } catch (e) {
+      next(e);
+    }
   });
 
-  app.put("/campgrounds/:id/edit", async (req, res) => {
-    const { id } = req.params;
-    // TIP: req.body brings 'title' and 'location' wrapped on an 'campground' object:
-    // i.e. { campground: { title: 'Tumbling Creek', location: 'Jupiter, Florida' } }
-    // This is because of the 'name' property on edit.ejs
-    const campground = await Campground.findByIdAndUpdate(
-      id,
-      { ...req.body.campground },
-      { new: true }
-    );
-    // Why copy the campground? Because I will probably add more attributes later when editing.
-    console.log({ campground });
-    res.redirect(`/campgrounds/${campground._id}/show`);
+  app.put("/campgrounds/:id/edit", async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      // TIP: req.body brings 'title' and 'location' wrapped on an 'campground' object:
+      // i.e. { campground: { title: 'Tumbling Creek', location: 'Jupiter, Florida' } }
+      // This is because of the 'name' property on edit.ejs
+      const campground = await Campground.findByIdAndUpdate(
+        id,
+        { ...req.body.campground },
+        { new: true }
+      );
+      // Why copy the campground? Because I will probably add more attributes later when editing.
+      console.log({ campground });
+      res.redirect(`/campgrounds/${campground._id}/show`);
+    } catch (e) {
+      next(e);
+    }
   });
 
   // * DELETE CAMPGROUND
-  app.delete("/campgrounds/:id/delete", async (req, res) => {
-    const { id } = req.params;
-    const deletedCampground = await Campground.findByIdAndDelete(id);
-    console.log({ deletedCampground });
-    res.redirect("/campgrounds");
+  app.delete("/campgrounds/:id/delete", async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const deletedCampground = await Campground.findByIdAndDelete(id);
+      console.log({ deletedCampground });
+      res.redirect("/campgrounds");
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  app.all("*", (req, res, next) => {
+    next(new ExpressError("Page Not Found", 404));
+  });
+
+  app.use((err, req, res, next) => {
+    const { statusCode = 5000 } = err;
+    if (!err.message) err.message = "Something went wrong..."
+    res.status(statusCode).render("error", { err });
   });
 
   app.listen(3000, () => {
