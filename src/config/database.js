dbf987544a8d392b1d17a2a1462ced958b1972d3
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-01-15 16:40:03 +0100

    Modified database.js to use local MongoDB database/ Pending TODO to switch it more easilygit status

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,16 @@
 import mongoose from 'mongoose';
 
 export default async function (MONGO_URL, MONGO_DB_NAME) {
-  await mongoose.connect(MONGO_URL, {
-    dbName: MONGO_DB_NAME,
-  });
-  console.log(`Mongoose connected to ${MONGO_DB_NAME} database.`);
+  const local = true; // * Toggle this between local and cloud DB
+  // TODO: Can I control this when running npm script?
+
+  if (local) {
+    await mongoose.connect('mongodb://localhost:27017/yelp-camp-2024');
+    console.log(`Mongoose connected to LOCAL database.`);
+  } else {
+    await mongoose.connect(MONGO_URL, {
+      dbName: MONGO_DB_NAME,
+    });
+    console.log(`Mongoose connected to ${MONGO_DB_NAME} database.`);
+  }
 }
