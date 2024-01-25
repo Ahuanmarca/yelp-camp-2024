f1912e66216bdbb6b07494bf4d0e30ed64d3a02c
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-01-25 08:43:26 +0100

    Click on unclustered point shows campground details and link to show page

--- src/models/Campground.js
+++ src/models/Campground.js
@@ -14,36 +14,47 @@ ImageSchema.virtual('thumbnail').get(function () {
   return this.url.replace('/upload', '/upload/w_200');
 });
 
-const CampgroundSchema = new Schema({
-  title: String,
-  // image: String, // TODO: Reimplement this later
-  // We need to follow geo-json pattern
-  geometry: {
-    type: {
-      type: String,
-      enum: ['Point'],  // 'geometry.type' must be 'Point'
-      required: true,
+const CampgroundSchema = new Schema(
+  {
+    title: String,
+    // image: String, // TODO: Reimplement this later
+    // We need to follow geo-json pattern
+    geometry: {
+      type: {
+        type: String,
+        enum: ['Point'], // 'geometry.type' must be 'Point'
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
-    }
-  },
-  images: [ImageSchema],
-  price: Number,
-  description: String,
-  location: String,
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  reviews: [
-    {
+    images: [ImageSchema],
+    price: Number,
+    description: String,
+    location: String,
+    author: {
       type: Schema.Types.ObjectId,
-      ref: 'Review',
+      ref: 'User',
     },
-  ],
-});
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Review',
+      },
+    ],
+  },
+  // Schema options
+  // Make virtuals available on the browser
+  { toJSON: { virtuals: true } }
+);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+  return `
+  <strong><a href="/campgrounds/${this._id}/show">${this.title}</a><strong>
+  <p>${this.description.substring(0, 20)}...</p>`
+})
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
   if (doc) {
