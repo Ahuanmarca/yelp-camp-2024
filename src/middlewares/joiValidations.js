89f33434476efd1365fdbf256df52196334a6b55
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-01-28 21:27:48 +0100

    Adding html sanitization

--- src/middlewares/joiValidations.js
+++ src/middlewares/joiValidations.js
@@ -1,13 +1,36 @@
-import Joi from 'joi';
+import BaseJoi from 'joi';
 import ExpressError from '../utils/ExpressError.js';
+import sanitizeHtml from 'sanitize-html';
+
+const extension = (joi) => ({
+  type: 'string',
+  base: joi.string(),
+  messages: {
+      'string.escapeHTML': '{{#label}} must not include HTML!'
+  },
+  rules: {
+      escapeHTML: {
+          validate(value, helpers) {
+              const clean = sanitizeHtml(value, {
+                  allowedTags: [],
+                  allowedAttributes: {},
+              });
+              if (clean !== value) return helpers.error('string.escapeHTML', { value })
+              return clean;
+          }
+      }
+  }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 const campgroundSchema = Joi.object({
   campground: Joi.object({
-    title: Joi.string().required(),
+    title: Joi.string().required().escapeHTML(),
     price: Joi.number().required().min(0),
     // image: Joi.string().required(), // <- bacause cloudinary / multer
-    location: Joi.string().required(),
-    description: Joi.string().required(),
+    location: Joi.string().required().escapeHTML(),
+    description: Joi.string().required().escapeHTML(),
   }).required(),
   deleteImages: Joi.array(),
 });
@@ -25,7 +48,7 @@ function validateCampground(req, res, next) {
 const reviewSchema = Joi.object({
   review: Joi.object({
     rating: Joi.number().required().min(1).max(5),
-    body: Joi.string().required(),
+    body: Joi.string().required().escapeHTML(),
   }).required(),
 });
 
